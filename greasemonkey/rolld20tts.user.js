444477ccab41857f31aeea4d497a66c2ab5e4d9d
ConsciousCode <conscious.code@gmail.com>
2019-06-08 00:40:58 -0600

    Parse HTML directly

--- greasemonkey/rolld20tts.user.js
+++ greasemonkey/rolld20tts.user.js
@@ -3,90 +3,124 @@
 // @version 2.0
 // @description roll20.net tts
 // @author ConsciousCode
+// @namespace https://github.com/ConsciousCode/ttsmonkey
 // @match *://roll20.net/*
 // @match https://app.roll20.net/editor/
-// @grant unsafeWindow
 // @run-at document-start
 // ==/UserScript==
 
 'use strict';
 
-// Source: https://github.com/kcaf/d20-boilerplate
-var d20boilerplate = function() {
-	const NAME = "d20-boilerplate";
-	console.log(NAME,"> Injected");
-
-	// A few common cleanups for the messages
-	function cleanWhat(what) {
-		return what.
-			replace(/\bjpg\b/g, "jpeg"). // without this it spells out j-p-g
-			replace(/\b(g?ui)\b/g, '<say-as interpret-as="characters">$2</say-as>').
-			replace(/(?:https?:\/\/)?((?:[-\w]+\.)+[-\w]+)(?:\/.+$)?|\b([?!()<>\[\]]+)\b|(<em>)|(<\/em>)|\.{2,}/g,
+// A few common cleanups for the messages
+function cleanWhat(what) {
+	return what.
+		replace(/\bjpg\b/g, "jpeg"). // without this it spells out j-p-g
+		replace(/\b(g?ui)\b/g, '<say-as interpret-as="characters">$2</say-as>').
+		replace(/(?:https?:\/\/)?((?:[-\w]+\.)+[-\w]+)(?:\/.+$)?|\b([?!()<>\[\]]+)\b|(<em>)|(<\/em>)|\.{2,}/g,
 			($0, $1, $2, $3, $4, $5) => {
-				if($1) return `<prosody rate="fast">${$1}</prosody>`;
-				if($2) return `<prosody rate="x-fast">${$2}</prosody>`;
-				if($3) return '<emphasis level="strong">';
-				if($4) return "</emphasis>";
-				if($5) return `<break strength="${
+				if ($1) return `<prosody rate="fast">${$1}</prosody>`;
+				if ($2) return `<prosody rate="x-fast">${$2}</prosody>`;
+				if ($3) return '<emphasis level="strong">';
+				if ($4) return "</emphasis>";
+				if ($5) return `<break strength="${
 					["x-weak", "weak", "medium", "strong", "x-strong"][$5.length - 2] || "x-strong"}"/>`;
 			});
-	}
+}
 
-	function say(what) {
-		ttsmonkey_say(cleanWhat(what), false);
-	}
+function say(what) {
+	ttsmonkey_say(cleanWhat(what), false);
+}
 
-	// Window loaded
-	window.onload = function() {
-		var checkLoaded = setInterval(function() {
-			if (!$("#loading-overlay").is(":visible")) {
-				clearInterval(checkLoaded);
-				Init();
-			}
-		}, 1000);
-	};
-
-	// Init, d20 variable exposed and views are loaded
-	function Init() {
-    let d20 = window.d20;
-		// Hook the incoming function which acts as the core updater for the chat
-		let inc = d20.textchat.incoming;
-		d20.textchat.incoming = function(b, msg, ...rest) {
-			if(msg.who !== "system" && msg.playerid !== d20_player_id) {
-				if(msg.type === "emote") {
-					say(msg.who + " " + msg.content);
-				}
-				else if(msg.type === "whisper") {
-					say(`${msg.who} whispered <emphasis level="reduced">${msg.content}</emphasis>`);
-				}
-				else if(msg.type === "diceroll") {
-					let roll = JSON.parse(msg.content);
-					say(`Rolling ${msg.sanitizedOrigRoll}. Total is ${roll.total}).`);
-				}
-				else if(msg.type === "error") {
-					say("Error: " + msg.content);
-				}
-				else {
-					say(msg.who + " said " + msg.content);
-				}
-			}
-
-			return inc.call(d20, b, msg, ...rest);
+function* immediateTextNodes(el) {
+	for (let c of el.children) {
+		if (c.nodeType === Node.TEXT_NODE) {
+			yield c;
 		}
-		console.log(NAME, "> Ready");
+	}
+}
+
+function trailingText(el) {
+	let x = "";
+	for (let y of immediateTextNodes(el)) {
+		x += y;
+	}
+	return x;
+}
+
+function handleMessage(type, who, msg) {
+	switch (type) {
+		case "general":
+			say(`${who} said ${msg}`);
+			break;
+		case "emote":
+			say(`${who} ${msg}`);
+			break;
+		case "whisper":
+			say(`${msg.who} whispered <emphasis level="reduced">${msg.content}</emphasis>`);
+			break;
+		case "diceroll":
+			say(`Rolling ${msg.formula}. Total is ${msg.rolled}).`);
+			break;
+		case "system": break;
+		case "error":
+			say(`Error: ${msg}`);
+			break;
+		default:
+			throw new Error(`Unknown message type "${type}"`);
+	}
+}
+
+function newMessage(el) {
+	let type, who, msg;
+
+	if (el.classList.contains("system")) {
+		type = "system";
+		who = null;
+		msg = null;
+	}
+	else {
+		msg = trailingText(el);
+
+		if (el.classList.contains("emote")) {
+			type = "emote";
+		}
+		else if (el.classList.contains("general")) {
+			type = "diceroll";
+			msg = {
+				formula: el.querySelector(".formula:not(.formattedFormula)"),
+				rolled: msg
+			};
+		}
+		else if (el.classList.contains("whisper") {
+			type = "whisper";
+			who = el.getElementsByClassName("by")[0].slice(0, -1);
+		}
+		else if (el.classList.contains("error") {
+			type = "error";
+		}
+		else {
+			throw new Error(`Unsupported message type(s): "${el.className}"`);
+		}
 	}
 
-  let lsgi = window.localStorage.getItem;
-  window.localStorage.getItem = new Proxy(lsgi, {
-    apply: function apply(target, self, args) {
-      if(args[0] === "WebRTC-AudioOutput") {
-        window.d20 = apply.caller.arguments[0];
-        console.log("> Obtained d20 variable");
-        window.localStorage.getItem = lsgi;
-      }
-    }
-  });
-};
+	handleMessage(type, who, msg);
+}
 
-// Inject
-unsafeWindow.eval(`(${d20boilerplate})()`);
+let ob = new MutationObserver(ls => {
+	for (let mut of ls) {
+		for (let el of mut.addedNodes) {
+			if (el.classList.contains("message")) {
+				try {
+					newMessage(el);
+				}
+				catch (e) {
+					alert(e);
+				}
+			}
+		}
+	}
+}).observe(document.getElementsByClassName("content")[0], {
+	attributes: false,
+	childList: true,
+	subtree: false
+});
