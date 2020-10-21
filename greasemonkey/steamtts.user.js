beb6f0edec4b8ddd815919320f749d3e7b11068c
ConsciousCode <conscious.code@gmail.com>
2020-10-21 01:29:25 -0600

    g_FriendsUIApp assignment was delayed, added setter to get around it

--- greasemonkey/steamtts.user.js
+++ greasemonkey/steamtts.user.js
@@ -5,45 +5,56 @@
 // @author ConsciousCode
 // @match *://steamcommunity.com/chat/
 // @grant unsafeWindow
-// @run-at document-end
+// @run-at document-start
 // ==/UserScript==
 
 unsafeWindow.eval("(" + (function() {
-'use strict';
-
-console.log("websteamtts BEGIN");
-
-function say(what) {
-	ttsmonkey_say(what);
-}
-
-function hook(obj, fn, hook) {
-	let v = obj[fn];
-	obj[fn] = function(...args) {
-		hook.apply(this, args);
-		return v.apply(this, args);
+	'use strict';
+	
+	console.log("websteamtts BEGIN");
+	
+	function say(what) {
+		ttsmonkey_say(what);
 	}
-}
-
-hook(g_FriendsUIApp, "OnReadyToRender", () => {
-	console.log("websteamtts OnReadyToRender");
-
-	let root = g_FriendsUIApp.UIStore.GetRootChatPerContextData();
-	let tabs = root.default_tabset.tabs;
-
-	tabs.observe(change => {
-		for(let tab of change.added) {
-			hook(tab.m_chat, "OnReceivedNewMessage", msg => {
-				let who = g_FriendsUIApp.FriendStore.GetFriend(msg.unAccountID);
-				say(
-					(who.nickname || who.persona.m_strPlayerName) +
-					" said " + msg.strMessage
-				);
+	
+	function hook(obj, fn, hook) {
+		let v = obj[fn];
+		obj[fn] = function(...args) {
+			hook.apply(this, args);
+			return v.apply(this, args);
+		}
+	}
+	
+	let myui;
+	
+	Object.defineProperty(window, 'g_FriendsUIApp', {
+		get() { return myui; },
+		set(v) {
+			myui = v;
+	
+			hook(myui, "OnReadyToRender", () => {
+				console.log("websteamtts OnReadyToRender");
+	
+				let root = myui.UIStore.GetRootChatPerContextData();
+				let tabs = root.default_tabset.tabs;
+	
+				tabs.observe(change => {
+					console.log("websteamtts Tab changed");
+					for(let tab of change.added) {
+						hook(tab.m_chat, "OnReceivedNewMessage", msg => {
+							console.log("websteamtts New message");
+							let who = myui.FriendStore.GetFriend(msg.unAccountID);
+							say(
+								(who.nickname || who.persona.m_strPlayerName) +
+								" said " + msg.strMessage
+							);
+						});
+					}
+				})
 			});
 		}
-	})
-});
-
-// g_FriendsUIApp.FriendStore.m_self
-
-}).toString() + ")()");
+	});
+	// g_FriendsUIApp.FriendStore.m_self
+	
+	}).toString() + ")()");
+	
