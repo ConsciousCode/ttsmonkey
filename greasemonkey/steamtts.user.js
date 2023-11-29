8318f64a1541cb62fb8ff7f9e5806906f19474cf
ConsciousCode <conscious.code@gmail.com>
2023-11-28 22:52:34 -0700

    2-phase mutation observation

--- greasemonkey/steamtts.user.js
+++ greasemonkey/steamtts.user.js
@@ -1,18 +1,21 @@
 // ==UserScript==
 // @name websteamtts
-// @version 0.1
+// @version 0.2
 // @description TTS for Steam web chat client
 // @author ConsciousCode
 // @match *://steamcommunity.com/chat/
 // @grant unsafeWindow
-// @run-at document-start
+// @run-at document-end
 // ==/UserScript==
 
 unsafeWindow.eval("(" + (function() {
 	'use strict';
 	
-	document.addEventListener("DOMContentLoaded", () => {
-		console.log("websteamtts BEGIN v2");
+	console.log("websteamtts BEGIN v3");
+	
+	/* Hook into the page once it's actually loaded. */
+	function hook(chatHistory) {
+		console.log("websteamtts: hook");
 		
 		function parseMessage(el) {
 			const name = el.querySelector('.speakerName').textContent;
@@ -22,27 +25,37 @@ unsafeWindow.eval("(" + (function() {
 			return {name, timestamp, content};
 		}
 		
-		const chatHistory = document.querySelector('.chatHistory');
-		console.log("websteamtts: chatHistory", chatHistory);
-		if(chatHistory) {
-			new MutationObserver(muts => {
-				console.log("websteamtts: mutation")
-				for(const mut of muts) {
-					if(mut.addedNodes.length == 0) continue;
-					
-					for(const node of mut.addedNodes) {
-						if(node.classList && node.classList.contains('ChatMessageBlock')) {
-							const msg = parseMessage(node);
-							ttsmonkey_say(`${msg.name} says ${msg.content}`);
-						}
+		new MutationObserver(muts => {
+			console.log("websteamtts: mutation")
+			for(const mut of muts) {
+				if(mut.addedNodes.length == 0) continue;
+				
+				for(const node of mut.addedNodes) {
+					if(node.classList && node.classList.contains('ChatMessageBlock')) {
+						const msg = parseMessage(node);
+						ttsmonkey_say(`${msg.name} says ${msg.content}`);
 					}
 				}
-			}).observe(chatHistory, { childList: true });
+			}
+		}).observe(chatHistory, { childList: true });
+	}
+	
+	/* Wait for Steam React to load. */
+	new MutationObserver((muts, observer) => {
+		console.log("websteamtts: global mutation")
+		for(var mut of muts) {
+			if(mut.type === 'childList') {
+				const chatHistory = document.querySelector('.chatHistory');
+				console.log("websteamtts: chatHistory", chatHistory);
+				if(chatHistory) {
+					// Element is now present, call your function
+					hook(chatHistory);
+					observer.disconnect();
+					break;
+				}
+			}
 		}
-		else {
-			throw new Error("websteamtts: `.chatHistory` not found");
-		}
-	});
+	}).observe(document.body, { childList: true, subtree: true });
 	
 	}).toString() + ")()");
 	
