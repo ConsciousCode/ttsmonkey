b700a8481ab6ed2f835b54fa7bc4acb1e7bea1b0
ConsciousCode <conscious.code@gmail.com>
2023-11-28 23:02:22 -0700

    Don't read current user messages

--- greasemonkey/steamtts.user.js
+++ greasemonkey/steamtts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name websteamtts
-// @version 0.2
+// @version 0.4
 // @description TTS for Steam web chat client
 // @author ConsciousCode
 // @match *://steamcommunity.com/chat/
@@ -11,7 +11,7 @@
 unsafeWindow.eval("(" + (function() {
 	'use strict';
 	
-	console.log("websteamtts BEGIN v3");
+	console.log("websteamtts BEGIN v4");
 	
 	/* Hook into the page once it's actually loaded. */
 	function hook(chatHistory) {
@@ -21,8 +21,9 @@ unsafeWindow.eval("(" + (function() {
 			const name = el.querySelector('.speakerName').textContent;
 			const timestamp = el.querySelector('.speakerTimeStamp').textContent;
 			const content = el.querySelector('.msgText').textContent;
-		
-			return {name, timestamp, content};
+			const isCurrentUser = el.classList.contains('isCurrentUser');
+			
+			return {name, timestamp, content, isCurrentUser};
 		}
 		
 		new MutationObserver(muts => {
@@ -33,7 +34,9 @@ unsafeWindow.eval("(" + (function() {
 				for(const node of mut.addedNodes) {
 					if(node.classList && node.classList.contains('ChatMessageBlock')) {
 						const msg = parseMessage(node);
-						ttsmonkey_say(`${msg.name} says ${msg.content}`);
+						if(!msg.isCurrentUser) {
+							ttsmonkey_say(`${msg.name} says ${msg.content}`);
+						}
 					}
 				}
 			}
