4f09b00a7f99b288c261fbf4341193192ba7adcf
ConsciousCode <conscious.code@gmail.com>
2023-11-28 22:32:49 -0700

    Defer to DOMContentLoaded

--- greasemonkey/steamtts.user.js
+++ greasemonkey/steamtts.user.js
@@ -11,36 +11,38 @@
 unsafeWindow.eval("(" + (function() {
 	'use strict';
 	
-	console.log("websteamtts BEGIN - new");
-	
-	function parseMessage(el) {
-		const name = el.querySelector('.speakerName').textContent;
-		const timestamp = el.querySelector('.speakerTimeStamp').textContent;
-		const content = el.querySelector('.msgText').textContent;
-	
-		return {name, timestamp, content};
-	}
-	
-	const chatHistory = document.querySelector('.chatHistory');
-	console.log("websteamtts: chatHistory", chatHistory);
-	if(chatHistory) {
-		new MutationObserver(muts => {
-			console.log("websteamtts: mutation")
-			for(const mut of muts) {
-				if(mut.addedNodes.length == 0) continue;
-				
-				for(const node of mut.addedNodes) {
-					if(node.classList && node.classList.contains('ChatMessageBlock')) {
-						const msg = parseMessage(node);
-						ttsmonkey_say(`${msg.name} says ${msg.content}`);
+	document.addEventListener("DOMContentLoaded", () => {
+		console.log("websteamtts BEGIN v2");
+		
+		function parseMessage(el) {
+			const name = el.querySelector('.speakerName').textContent;
+			const timestamp = el.querySelector('.speakerTimeStamp').textContent;
+			const content = el.querySelector('.msgText').textContent;
+		
+			return {name, timestamp, content};
+		}
+		
+		const chatHistory = document.querySelector('.chatHistory');
+		console.log("websteamtts: chatHistory", chatHistory);
+		if(chatHistory) {
+			new MutationObserver(muts => {
+				console.log("websteamtts: mutation")
+				for(const mut of muts) {
+					if(mut.addedNodes.length == 0) continue;
+					
+					for(const node of mut.addedNodes) {
+						if(node.classList && node.classList.contains('ChatMessageBlock')) {
+							const msg = parseMessage(node);
+							ttsmonkey_say(`${msg.name} says ${msg.content}`);
+						}
 					}
 				}
-			}
-		}).observe(chatHistory, { childList: true });
-	}
-	else {
-		throw new Error("websteamtts: `.chatHistory` not found");
-	}
+			}).observe(chatHistory, { childList: true });
+		}
+		else {
+			throw new Error("websteamtts: `.chatHistory` not found");
+		}
+	});
 	
 	}).toString() + ")()");
 	
