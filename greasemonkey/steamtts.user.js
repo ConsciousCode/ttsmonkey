5ea58a426c14b041243714dd10553727be3744fa
ConsciousCode <conscious.code@gmail.com>
2023-12-02 20:32:30 -0700

    Wrong variable name

--- greasemonkey/steamtts.user.js
+++ greasemonkey/steamtts.user.js
@@ -1,60 +1,73 @@
 // ==UserScript==
 // @name websteamtts
-// @version 0.1
+// @version 0.8
 // @description TTS for Steam web chat client
 // @author ConsciousCode
 // @match *://steamcommunity.com/chat/
 // @grant unsafeWindow
-// @run-at document-start
+// @run-at document-end
 // ==/UserScript==
 
 unsafeWindow.eval("(" + (function() {
 	'use strict';
 	
-	console.log("websteamtts BEGIN");
+	console.log("websteamtts BEGIN v8");
 	
-	function say(what) {
-		ttsmonkey_say(what);
-	}
-	
-	function hook(obj, fn, hook) {
-		let v = obj[fn];
-		obj[fn] = function(...args) {
-			hook.apply(this, args);
-			return v.apply(this, args);
-		}
-	}
-	
-	let myui;
-	
-	Object.defineProperty(window, 'g_FriendsUIApp', {
-		get() { return myui; },
-		set(v) {
-			myui = v;
-	
-			hook(myui, "OnReadyToRender", () => {
-				console.log("websteamtts OnReadyToRender");
-	
-				let root = myui.UIStore.GetRootChatPerContextData();
-				let tabs = root.default_tabset.tabs;
-	
-				tabs.observe(change => {
-					console.log("websteamtts Tab changed");
-					for(let tab of change.added) {
-						hook(tab.m_chat, "OnReceivedNewMessage", msg => {
-							console.log("websteamtts New message");
-							let who = myui.FriendStore.GetFriend(msg.unAccountID);
-							say(
-								(who.nickname || who.persona.m_strPlayerName) +
-								" said " + msg.strMessage
-							);
-						});
+	/* Hook into the page once it's actually loaded. */
+	function hook(chatHistory) {
+		console.log("websteamtts: hook");
+		
+		let initialLoad = true; // debounce initial load
+		new MutationObserver(muts => {
+			if(initialLoad) {
+				initialLoad = false;
+				return;
+			}
+			console.log("websteamtts: mutation");
+			
+			for(const mut of muts) {
+				if(mut.addedNodes.length == 0) continue;
+				
+				for(const node of mut.addedNodes) {
+					if(!node.classList) continue;
+					
+					if(node.classList.contains('ChatMessageBlock')) {
+						// Don't read our own messages
+						if(node.querySelector(".ChatSpeaker.isCurrentUser") !== null) {
+							continue;
+						}
+						
+						const name = node.querySelector('.speakerName').textContent;
+						
+						// Gather unread messages
+						const unread = [];
+						for(const msg of node.querySelectorAll(".msgText:not(.ttsmonkey-seen)")) {
+							msg.classList.add("ttsmonkey-seen");
+							unread.push(msg.textContent);
+						}
+						console.log("websteamtts: unread", unread);
+						ttsmonkey_say(`${name} says ${unread.join("\n")}`);
 					}
-				})
-			});
+				}
+			}
+		}).observe(chatHistory, { childList: true });
+	}
+	
+	/* Wait for Steam React to load. */
+	new MutationObserver((muts, observer) => {
+		console.log("websteamtts: global mutation")
+		for(var mut of muts) {
+			if(mut.type === 'childList') {
+				const chatHistory = document.querySelector('.chatHistory');
+				console.log("websteamtts: chatHistory", chatHistory);
+				if(chatHistory) {
+					// Element is now present, call your function
+					hook(chatHistory);
+					break;
+				}
+			}
 		}
-	});
-	// g_FriendsUIApp.FriendStore.m_self
+	}).observe(document.body, { childList: true, subtree: true });
 	
 	}).toString() + ")()");
 	
